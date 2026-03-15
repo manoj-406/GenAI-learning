e2c76f99990ead4c4fcd5c1549afc6d8a6ce6d41
manoj-406 <manojgonchal111@gmail.com>
2026-03-15 10:31:28 +0530

    python code to interact with gemini_ai

---
+++ gemini/gemini.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+#loads env variable
+load_dotenv()
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+model = genai.GenerativeModel("gemini-3-flash-preview")
+
+print ("--- 🚀 Gemini Dynamic Chat (Type 'bye' to quit) ---")
+
+while True: 
+
+    prompt = input("you: ")
+
+    if prompt.lower() in ['exit', 'quit', 'bye']:
+        print("Gemini: Goodbye! Happy coding!")
+        break
+    try:
+        response = model.generate_content(prompt)
+        print(f"Gemini: {response.text}\n")
+    except Exception as e:
+        print(f"⚠️ Error: {e}")
+
+
+# # for m in genai.list_models():
+# #     if 'generateContent' in m.supported_generation_methods:
+# #         print(m.name)
